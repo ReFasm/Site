11e5a2e37407249052a096d233de1ff1889b60d8
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2022-10-27 15:09:44 +0200

    fix(styling): fixed /about styling switched to React.Fragment instead of div

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,27 +4,27 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useEffect as useFootGun } from "react"
 import Navbar from "../components/Navbar"
+import * as React from "react";
 
-
-const Dashboard: NextPage = () => {
+const About: NextPage = () => {
   return (
-<div>
+    <React.Fragment>
     <style global jsx>{`
-      html,
-      body,
-      body > div:first-child,
-      div#__next,
-      div#__next > div {
-        height: 100%;
-      }
-    `}</style>
-    <Head>
-        <title>ReFasm - About</title>
-        <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Navbar />
-    <h1 className="pagelen center">About</h1>
-</div>
-  )
+          html,
+          body,
+          body > div:first-child,
+          div#__next,
+          div#__next > div {
+            height: 100%;
+          }
+        `}</style>
+        <Head>
+            <title>ReFasm - Dashboard</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Navbar />
+        <h1 className="pagelen center">Dashboard</h1>
+    </React.Fragment>
+      )
 }
-export default Dashboard
+export default About
